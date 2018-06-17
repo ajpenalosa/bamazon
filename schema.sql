3f06b5e0359fc627424fb9ae05309cad4f5e9d7c
Anthony Penalosa <aj@anthonys-mbp.attlocal.net>
2018-06-16 21:59:15 -0700

    Creating schema

---
+++ schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+	item_id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(100),
+    price DECIMAL(10,2),
+    stock_quantity INT
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES
+( "Jenga GIANT", "Toys & Games", 119.95, 10),
+( "Monopoly", "Toys & Games", 13.59, 12),
+( "Operation", "Toys & Games", 14.79, 5),
+( "Super Smash Bros. Ultimate", "Video Games", 59.99, 4),
+( "State of Decay 2", "Video Games", 29.88, 2),
+( "Fort Nite", "Video Games", 59.99, 3),
+( "Black Panther", "Movies", 17.00, 6),
+( "Justice League", "Movies", 10.00, 7),
+( "Jumanji", "Movies", 10.10, 2),
+( "Sony KD60X690E 60-Inch 4K Ultra HD Smart LED TV", "Television & Video", 698.00, 10),
+( "LG Electronics 65UJ7700 65-Inch 4K Ultra HD Smart LED TV", "Television & Video", 939.00, 10),
+( "Samsung 65NU8000 Flat 65-Inch 4K UHD 8 Series Smart LED TV", "Television & Video", 1397.99, 10)
